343bdd3679ae9eb489505f5f18db482e8980f20e
rindonbrg <140368244+rindonbrg@users.noreply.github.com>
2023-07-23 21:26:21 -0700

    Delete ResizeConsole.ps1

--- ResizeConsole.ps1
+++
@@ -1,4 +0,0 @@
-$width=80
-$height=24
-$Host.UI.RawUI.BufferSize = New-Object Management.Automation.Host.Size ($width, $height)
-$host.UI.RawUI.WindowSize = New-Object -TypeName System.Management.Automation.Host.Size -ArgumentList ($width, $height)
